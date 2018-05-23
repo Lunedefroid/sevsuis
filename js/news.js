b6b49d4ff9f8a3d1c15ba0dee236b0195c2bc779
eyemerf <eyemerf@gmail.com>
2018-05-23 21:14:55 +0300

    добавила всю свою папку с компа

--- js/news.js
+++ js/news.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
   $(".news_block").click(function(){
 $(".backgd").fadeIn(300);
 $(".news_window").fadeIn(200);
+
 return false;
 });
-$(".left_close, .window_coverbox").click(function(){
+$(".backgd, .window_coverbox").click(function(){
   $(".news_window").hide();
   $(".backgd").fadeOut(300);
 });
---
+++ kaf_name_src.js
@@ -0,0 +1,2 @@
+// var kaf_name = document.querySelectorAll('.kafedra_name');
+// toUpperCase(kaf_name);
