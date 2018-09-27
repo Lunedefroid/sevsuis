2ce11a23cc8c09a962be9d419366640b261ae782
Sektum <eyemerf@users.noreply.github.com>
2018-09-27 21:37:15 +0300

    27.09
    последняя версия

--- js/slider.js
+++ js/slider.js
@@ -3,5 +3,12 @@ $(document).on('ready', function(){
          dots: true,
          horizontal: true, //направление листания
          centerMode: false, //части соседних новостей
+         autoplay: true,
+         autoplaySpeed: 5000,
+         focusOnSelect: true,
+         infinite: true,
+         pauseOnHover: true,
+         pauseOnDotsHover: true,
+         adapiveHeight: true
        });
 });
